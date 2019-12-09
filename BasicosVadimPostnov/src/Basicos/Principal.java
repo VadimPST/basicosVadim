051643798c39ff3a8fbf695f421fa687510abee2
VadimPST <44578292+VadimPST@users.noreply.github.com>
2019-12-09 21:00:14 +0100

    Start

--- BasicosVadimPostnov/src/Basicos/Principal.java
+++ BasicosVadimPostnov/src/Basicos/Principal.java
@@ -6,30 +6,11 @@ public class Principal {
 		// TODO Auto-generated method stub
 		
 		
-		numericos();  // Llamada al método
+	//	numericos();  // Llamada al método
 
 	}
 	
 	
-	public static void numericos() {
-
-        //Estos son los tipos básicos
-        byte elMasPeque;
-        short chiquitin;
-        int entero;
-        long duplicaEntero;
-        float grande;
-        double doble;
-
-        //Ejemplos de asignación de valores a las variables de tipos básicos
-        entero = 10;
-        doble = 5.6;
-        //mas detalle
-        //Mostrar en pantalla el contendido de las variables de tipos básicos.
-        System.err.print("Valor entero ");
-        System.out.println(entero);
-        System.out.print("Valor doble ");
-        System.out.println(doble);
-    }
+	
 
 }
