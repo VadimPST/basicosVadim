69713bd72f91d08b5db5b86fe8f654d519e8b98d
VadimPST <44578292+VadimPST@users.noreply.github.com>
2019-12-11 13:46:57 +0100

    logicos changed

--- BasicosVadimPostnov/src/Basicos/Principal.java
+++ BasicosVadimPostnov/src/Basicos/Principal.java
@@ -6,10 +6,36 @@ public class Principal {
 		// TODO Auto-generated method stub
 		
 		
-	//	numericos();  // Llamada al método
-
+		logicos();//llamada al metodo de logicos
+ 
 	}
 	
+	
+	public static void logicos() {
+		
+		boolean variableLogica; //declaracion
+		
+		
+		variableLogica=true;//asignacion de valor
+		
+		
+		imprimeBoolean(variableLogica);//visualizacion mediante metodo creado
+		
+		
+		
+		
+	}
+	
+	public static void imprimeBoolean(boolean variable) {
+	
+		if(variable) {
+			System.out.println("la variable boolenana es TRUE");
+		}else {
+			System.out.println("la variable boolenana es FALSE");
+		}
+		
+		
+	}
 	
 	
 
